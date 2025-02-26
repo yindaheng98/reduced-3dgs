721a3850711dd26a5b3e71ef0ded65d09d047f23
yindaheng98 <yindaheng98@gmail.com>
2025-02-26 10:17:46 -0800

    compute SH according to self._degrees

--- reduced_3dgs/shculling/gaussian_model.py
+++ reduced_3dgs/shculling/gaussian_model.py
@@ -1,9 +1,5 @@
 import torch
-import math
-from gaussian_splatting import GaussianModel, Camera
-
-from gaussian_splatting.diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
-from gaussian_splatting.simple_knn._C import distCUDA2
+from gaussian_splatting import GaussianModel
 
 
 class VariableSHBandsGaussianModel(GaussianModel):
@@ -18,6 +14,20 @@ class VariableSHBandsGaussianModel(GaussianModel):
     @property
     def get_features(self):
         features_dc = self._features_dc
-        features_rest = self._features_rest
-        # TODO: compute SH according to self._degrees
+        # compute SH according to self._degrees
+        n_SH = (self._degrees + 1) ** 2 - 1
+        indices = torch.arange(n_SH.max(), device=n_SH.device).repeat(n_SH.shape[0], 1) < n_SH.unsqueeze(-1)
+        features_rest = torch.zeros_like(self._features_rest)
+        features_rest[indices, :] = self._features_rest[indices, :]
         return torch.cat((features_dc, features_rest), dim=1)
+
+    def init_degrees(self):
+        self._degrees = torch.zeros(self._xyz.shape[0], dtype=torch.int, device=self._xyz.device) + self.max_sh_degree
+
+    def create_from_pcd(self, *args, **kwargs):
+        super().create_from_pcd(*args, **kwargs)
+        self.init_degrees()
+
+    def load_ply(self, *args, **kwargs):
+        super().load_ply(*args, **kwargs)
+        self.init_degrees()
