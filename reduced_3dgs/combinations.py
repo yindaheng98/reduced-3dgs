7e89351b37e1e2d6cbf1a84798b763e071c93b1a
yindaheng98 <yindaheng98@gmail.com>
2025-03-01 16:32:37 -0800

    trainable camera

--- reduced_3dgs/combinations.py
+++ reduced_3dgs/combinations.py
@@ -1,7 +1,9 @@
-from gaussian_splatting import GaussianModel
-from gaussian_splatting.dataset import CameraDataset
-from gaussian_splatting.trainer import OpacityResetDensificationTrainer, OpacityResetter
-from reduced_3dgs.shculling import VariableSHGaussianModel, SHCuller
+from gaussian_splatting import GaussianModel, CameraTrainableGaussianModel, Camera
+from gaussian_splatting.dataset import CameraDataset, JSONCameraDataset, TrainableCameraDataset
+from gaussian_splatting.utils import psnr
+from gaussian_splatting.dataset.colmap import ColmapCameraDataset, colmap_init
+from gaussian_splatting.trainer import AbstractTrainer, OpacityResetDensificationTrainer, OpacityResetter, CameraOptimizer
+from reduced_3dgs.shculling import VariableSHGaussianModel, SHCuller, BaseSHCullingTrainer
 from reduced_3dgs.pruning import BasePruningTrainer, BasePrunerInDensifyTrainer
 from reduced_3dgs.shculling import VariableSHGaussianModel
 
@@ -73,4 +75,109 @@ def SHCullingPruningDensifyTrainer(
         cdist_threshold=cdist_threshold,
         std_threshold=std_threshold,
         cull_at_steps=cull_at_steps,
+    )
+
+
+class CameraTrainableVariableSHGaussianModel(VariableSHGaussianModel):
+    def forward(self, camera: Camera):
+        return CameraTrainableGaussianModel.forward(self, camera)
+
+
+def _CameraTrainerWrapper(
+    base_trainer_constructor,
+        model: CameraTrainableGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        camera_position_lr_init=0.00016,
+        camera_position_lr_final=0.0000016,
+        camera_position_lr_delay_mult=0.01,
+        camera_position_lr_max_steps=30_000,
+        camera_rotation_lr_init=0.0001,
+        camera_rotation_lr_final=0.000001,
+        camera_rotation_lr_delay_mult=0.01,
+        camera_rotation_lr_max_steps=30_000,
+        *args, **kwargs):
+    return CameraOptimizer(
+        base_trainer_constructor(model, scene_extent, dataset, *args, **kwargs),
+        dataset, scene_extent,
+        camera_position_lr_init=camera_position_lr_init,
+        camera_position_lr_final=camera_position_lr_final,
+        camera_position_lr_delay_mult=camera_position_lr_delay_mult,
+        camera_position_lr_max_steps=camera_position_lr_max_steps,
+        camera_rotation_lr_init=camera_rotation_lr_init,
+        camera_rotation_lr_final=camera_rotation_lr_final,
+        camera_rotation_lr_delay_mult=camera_rotation_lr_delay_mult,
+        camera_rotation_lr_max_steps=camera_rotation_lr_max_steps
+    )
+
+
+def CameraSHCullingTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return _CameraTrainerWrapper(
+        BaseSHCullingTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraPruningTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return _CameraTrainerWrapper(
+        BasePruningTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraOpacityResetPrunerInDensifyTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return _CameraTrainerWrapper(
+        OpacityResetPrunerInDensifyTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraSHCullingDensifyTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return _CameraTrainerWrapper(
+        SHCullingDensifyTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraSHCullingPruneTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return _CameraTrainerWrapper(
+        SHCullingPruneTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
+    )
+
+
+def CameraSHCullingPruningDensifyTrainer(
+        model: CameraTrainableVariableSHGaussianModel,
+        scene_extent: float,
+        dataset: TrainableCameraDataset,
+        *args, **kwargs):
+    return _CameraTrainerWrapper(
+        SHCullingPruningDensifyTrainer,
+        model, scene_extent, dataset,
+        *args, **kwargs
     )
--- reduced_3dgs/train.py
+++ reduced_3dgs/train.py
@@ -5,16 +5,18 @@ import torch
 from tqdm import tqdm
 from argparse import Namespace
 from gaussian_splatting import GaussianModel
-from gaussian_splatting.dataset import CameraDataset, JSONCameraDataset
+from gaussian_splatting.dataset import CameraDataset, JSONCameraDataset, TrainableCameraDataset
 from gaussian_splatting.utils import psnr
-from gaussian_splatting.dataset.colmap import ColmapCameraDataset, colmap_init
+from gaussian_splatting.dataset.colmap import ColmapCameraDataset, ColmapTrainableCameraDataset, colmap_init
 from gaussian_splatting.trainer import AbstractTrainer
 from reduced_3dgs.shculling import VariableSHGaussianModel, BaseSHCullingTrainer
 from reduced_3dgs.pruning import BasePruningTrainer
 from reduced_3dgs.combinations import OpacityResetPrunerInDensifyTrainer, SHCullingDensifyTrainer, SHCullingPruneTrainer, SHCullingPruningDensifyTrainer
+from reduced_3dgs.combinations import CameraTrainableVariableSHGaussianModel, CameraSHCullingTrainer, CameraPruningTrainer
+from reduced_3dgs.combinations import CameraOpacityResetPrunerInDensifyTrainer, CameraSHCullingDensifyTrainer, CameraSHCullingPruneTrainer, CameraSHCullingPruningDensifyTrainer
 
 
-modes = {
+basemodes = {
     "shculling": BaseSHCullingTrainer,
     "pruning": BasePruningTrainer,
     "densify-pruning": OpacityResetPrunerInDensifyTrainer,
@@ -22,18 +24,39 @@ modes = {
     "prune-shculling": SHCullingPruneTrainer,
     "densify-prune-shculling": SHCullingPruningDensifyTrainer,
 }
+cameramodes = {
+    "camera-shculling": CameraSHCullingTrainer,
+    "camera-pruning": CameraPruningTrainer,
+    "camera-densify-pruning": CameraOpacityResetPrunerInDensifyTrainer,
+    "camera-densify-shculling": CameraSHCullingDensifyTrainer,
+    "camera-prune-shculling": CameraSHCullingPruneTrainer,
+    "camera-densify-prune-shculling": CameraSHCullingPruningDensifyTrainer,
+}
 
 
 def prepare_training(sh_degree: int, source: str, device: str, mode: str, load_ply: str = None, load_camera: str = None, configs={}) -> Tuple[CameraDataset, GaussianModel, AbstractTrainer]:
-    gaussians = VariableSHGaussianModel(sh_degree).to(device)
-    gaussians.load_ply(load_ply) if load_ply else colmap_init(gaussians, source)
-    dataset = (JSONCameraDataset(load_camera) if load_camera else ColmapCameraDataset(source)).to(device)
-    trainer = modes[mode](
-        gaussians,
-        scene_extent=dataset.scene_extent(),
-        dataset=dataset,
-        **configs
-    )
+    if mode in basemodes:
+        gaussians = VariableSHGaussianModel(sh_degree).to(device)
+        gaussians.load_ply(load_ply) if load_ply else colmap_init(gaussians, source)
+        dataset = (JSONCameraDataset(load_camera) if load_camera else ColmapCameraDataset(source)).to(device)
+        trainer = basemodes[mode](
+            gaussians,
+            scene_extent=dataset.scene_extent(),
+            dataset=dataset,
+            **configs
+        )
+    elif mode in cameramodes:
+        gaussians = CameraTrainableVariableSHGaussianModel(sh_degree).to(device)
+        gaussians.load_ply(load_ply) if load_ply else colmap_init(gaussians, source)
+        dataset = (TrainableCameraDataset.from_json(load_camera) if load_camera else ColmapTrainableCameraDataset(source)).to(device)
+        trainer = cameramodes[mode](
+            gaussians,
+            scene_extent=dataset.scene_extent(),
+            dataset=dataset,
+            **configs
+        )
+    else:
+        raise ValueError(f"Unknown mode: {mode}")
     return dataset, gaussians, trainer
 
 
@@ -82,7 +105,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--iteration", default=30000, type=int)
     parser.add_argument("-l", "--load_ply", default=None, type=str)
     parser.add_argument("--load_camera", default=None, type=str)
-    parser.add_argument("--mode", choices=list(modes.keys()), default="densify-prune-shculling")
+    parser.add_argument("--mode", choices=list(basemodes.keys()) + list(cameramodes.keys()), default="densify-prune-shculling")
     parser.add_argument("--save_iterations", nargs="+", type=int, default=[7000, 30000])
     parser.add_argument("--device", default="cuda", type=str)
     parser.add_argument("-o", "--option", default=[], action='append', type=str)
