64b78696b64b9c1dbbb849d80359c18d6936d74b
yindaheng98 <yindaheng98@gmail.com>
2025-03-04 20:53:34 -0800

    fixed Trainer

--- reduced_3dgs/quantization/quantizer.py
+++ reduced_3dgs/quantization/quantizer.py
@@ -218,14 +218,29 @@ class VectorQuantizer(AbstractQuantizer):
 
 
 def VectorQuantizeTrainerWrapper(
-        base_trainer: AbstractTrainer,
+    base_trainer: AbstractTrainer,
         num_clusters=256,
+        num_clusters_rotation_re=None,
+        num_clusters_rotation_im=None,
+        num_clusters_opacity=None,
+        num_clusters_scaling=None,
+        num_clusters_features_dc=None,
+        num_clusters_features_rest=[],
         quantizate_from_iter=5000,
         quantizate_until_iter=30000,
         quantizate_interval=500,
 ):
     return QuantizeTrainerWrapper(
-        base_trainer, VectorQuantizer(base_trainer.model, num_clusters=num_clusters),
+        base_trainer, VectorQuantizer(
+            base_trainer.model,
+            num_clusters=num_clusters,
+            num_clusters_rotation_re=num_clusters_rotation_re,
+            num_clusters_rotation_im=num_clusters_rotation_im,
+            num_clusters_opacity=num_clusters_opacity,
+            num_clusters_scaling=num_clusters_scaling,
+            num_clusters_features_dc=num_clusters_features_dc,
+            num_clusters_features_rest=num_clusters_features_rest,
+        ),
         quantizate_from_iter, quantizate_until_iter, quantizate_interval
     )
 
@@ -234,12 +249,27 @@ def VectorQuantizeTrainer(
     model: GaussianModel,
     spatial_lr_scale: float,
         num_clusters=256,
+        num_clusters_rotation_re=None,
+        num_clusters_rotation_im=None,
+        num_clusters_opacity=None,
+        num_clusters_scaling=None,
+        num_clusters_features_dc=None,
+        num_clusters_features_rest=[],
         quantizate_from_iter=5000,
         quantizate_until_iter=30000,
         quantizate_interval=1000,
         *args, **kwargs):
     return QuantizeTrainerWrapper(
         BaseTrainer(model, spatial_lr_scale, *args, **kwargs),
-        VectorQuantizer(model, num_clusters=num_clusters),
+        VectorQuantizer(
+            model,
+            num_clusters=num_clusters,
+            num_clusters_rotation_re=num_clusters_rotation_re,
+            num_clusters_rotation_im=num_clusters_rotation_im,
+            num_clusters_opacity=num_clusters_opacity,
+            num_clusters_scaling=num_clusters_scaling,
+            num_clusters_features_dc=num_clusters_features_dc,
+            num_clusters_features_rest=num_clusters_features_rest,
+        ),
         quantizate_from_iter, quantizate_until_iter, quantizate_interval
     )
