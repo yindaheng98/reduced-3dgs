950b8c13b718bff3c7043325f577c83eeab000b3
yindaheng98 <yindaheng98@gmail.com>
2025-04-23 17:49:08 -0700

    better default config

--- reduced_3dgs/pruning/combinations.py
+++ reduced_3dgs/pruning/combinations.py
@@ -5,15 +5,41 @@ from gaussian_splatting.trainer import DepthTrainerWrapper
 from .trainer import BasePruningTrainer, BasePrunerInDensifyTrainer
 
 
-# Camera trainer
+# Depth trainer
 
 
-def DepthPruningTrainer(model: GaussianModel, scene_extent: float, dataset: TrainableCameraDataset, *args, depth_from_iter=500, **kwargs):
-    return DepthTrainerWrapper(BasePruningTrainer, model, scene_extent, *args, dataset=dataset, depth_from_iter=depth_from_iter, **kwargs)
+def DepthPruningTrainer(
+        model: GaussianModel, scene_extent: float, dataset: TrainableCameraDataset,
+        *args,
+        depth_from_iter=500,
+        depth_l1_weight_final=1.,
+        depth_resize=1024,
+        **kwargs):
+    return DepthTrainerWrapper(
+        BasePruningTrainer, model, scene_extent,
+        *args,
+        dataset=dataset,
+        depth_from_iter=depth_from_iter,
+        depth_l1_weight_final=depth_l1_weight_final,
+        depth_resize=depth_resize,
+        **kwargs)
 
 
-def DepthPrunerInDensifyTrainer(model: GaussianModel, scene_extent: float, dataset: TrainableCameraDataset, *args, depth_from_iter=500, **kwargs):
-    return DepthTrainerWrapper(BasePrunerInDensifyTrainer, model, scene_extent, *args, dataset=dataset, depth_from_iter=depth_from_iter, **kwargs)
+def DepthPrunerInDensifyTrainer(
+        model: GaussianModel, scene_extent: float, dataset: TrainableCameraDataset,
+        *args,
+        depth_from_iter=500,
+        depth_l1_weight_final=1.,
+        depth_resize=1024,
+        **kwargs):
+    return DepthTrainerWrapper(
+        BasePrunerInDensifyTrainer, model, scene_extent,
+        *args,
+        dataset=dataset,
+        depth_from_iter=depth_from_iter,
+        depth_l1_weight_final=depth_l1_weight_final,
+        depth_resize=depth_resize,
+        **kwargs)
 
 
 PruningTrainer = DepthPruningTrainer
